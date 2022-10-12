aa51358d350dcd7cd1080c34e3db0649aebd27eb
RomanChebotarev <RomanCh@artel.im>
2022-10-12 09:07:08 +0300

    Implement tests for logging

--- log_test.go
+++ log_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sort"
 )
 
 var tempDir string
@@ -41,17 +42,32 @@ func TestMain(m *testing.M) {
 
 
 func TestLogging(t *testing.T) {
-	for testN, test := range tests {
+	// Make sorted list of test names
+	testNames := make([]string, 0, len(tests))
+	for testN := range tests {
+		testNames = append(testNames, testN)
+	}
+	// Sort it
+	sort.Strings(testNames)
+
+	// Run tests
+	for _, testN := range testNames {
+		// Get test configuration
+		test := tests[testN]
+
 		// Create output filename
-		file := filepath.Join(tempDir, fmt.Sprintf("log-test-%02d.log", testN))
+		file := filepath.Join(tempDir, fmt.Sprintf("log-test_%s.log", testN))
+
+		// Reinit internal package's logger variable to reset any configured parameters
+		logger = NewLogger()
 
 		// Open log file
 		if err := Open(file, stubApp, test.flags); err != nil {
-			t.Errorf("[%d] cannot open test log file: %v", testN, err)
+			t.Errorf("[%s] cannot open test log file: %v", testN, err)
 			t.FailNow()
 		}
 		// Set predefined PID
-		SetPid(stubPID)
+		SetPID(stubPID)
 
 		// Call pre() if exists
 		if test.pre != nil {
@@ -59,13 +75,22 @@ func TestLogging(t *testing.T) {
 		}
 
 		// Run log functions from inputs
-		for _, input := range test.inputs {
+		for i, input := range test.inputs {
+			// Call forEach() if exists
+			if test.forEach != nil {
+				if err := test.forEach(i); err != nil {
+					t.Errorf("[%s:%d] test.forEach failed: %v", testN, i, err)
+					t.FailNow()
+				}
+			}
+
+			// Write intput to log
 			input.f(stubLogFormat, input.args...)
 		}
 
 		// Close opened file
 		if err := Close(); err != nil {
-			t.Errorf("[%d] cannot close test log file: %v", testN, err)
+			t.Errorf("[%s] cannot close test log file: %v", testN, err)
 			t.FailNow()
 		}
 
@@ -76,7 +101,7 @@ func TestLogging(t *testing.T) {
 		// Reading test output
 		data, err := os.ReadFile(file)
 		if err != nil {
-			t.Errorf("[%d] cannot read produced file: %v", testN, err)
+			t.Errorf("[%s] cannot read produced file: %v", testN, err)
 			t.FailNow()
 		}
 
@@ -86,7 +111,7 @@ func TestLogging(t *testing.T) {
 		// Normally, produced contains empty line at the end because the must have "\n" at the end
 		if len(produced) > 0 {
 			if last := produced[len(produced) - 1]; last != "" {
-				t.Errorf("[%d] log file %q does not terminated by new line", testN, file)
+				t.Errorf("[%s] log file %q does not end with a new line", testN, file)
 			} else {
 				// Remove empty line from produced
 				produced = produced[0:len(produced)-1]
@@ -97,7 +122,7 @@ func TestLogging(t *testing.T) {
 		for ln := 0; ln < len(test.expected); ln++ {
 			// Check that produced[ln] exists
 			if ln + 1 > len(produced) {
-				t.Errorf("[%d:%d] expected string %q but no other lines in the produced file %q",
+				t.Errorf("[%s:%d] expected string %q but no other lines in the produced file %q",
 					testN, ln, test.expected[ln], file)
 				// Skip the rest of the test
 				goto nextTest
@@ -105,28 +130,23 @@ func TestLogging(t *testing.T) {
 
 			// Compare produced and expected
 			if produced[ln] != test.expected[ln] {
-				t.Errorf("[%d:%d] want %q, got %q", testN, ln, test.expected[ln], produced[ln])
+				t.Errorf("[%s:%d] want %q, got %q", testN, ln, test.expected[ln], produced[ln])
 			}
 		}
 
 		// Check for unexpected lines
 		if len(produced) > len(test.expected) {
-			t.Errorf("[%d] extra lines were found in the produced file: %#v", testN, produced[len(test.expected):])
+			t.Errorf("[%s] extra lines were found in the produced file: %#v", testN, produced[len(test.expected):])
 		}
 
 		nextTest:
-
-		// Call post() if exists
-		if test.post != nil {
-			test.post()
-		}
 	}
 }
 
 //
 // log methods required only for testing
 //
-func (l *Logger) SetPid(pidStr string) {
+func (l *Logger) SetPID(pidStr string) {
 	// Do nothing if PID should not be shown
 	if l.logFlags & NoPID != 0 {
 		return
@@ -136,6 +156,6 @@ func (l *Logger) SetPid(pidStr string) {
 	l.logger.SetPrefix(fmt.Sprintf("%s[%s]: ", l.origPrefix, pidStr))
 
 }
-func SetPid(pidStr string) {
-	logger.SetPid(pidStr)
+func SetPID(pidStr string) {
+	logger.SetPID(pidStr)
 }
--- test_data_test.go
+++ test_data_test.go
@@ -1,21 +1,24 @@
 package log
 
+import "fmt"
+
 type logFunction func(string, ...any)
 type logCall struct {
 	f		logFunction
 	args	[]any
 }
 
-const stubLogFormat = `[#%d] %s log message`
+const stubLogFormat = `Test #%d - %s log message`
 
-var tests = []struct {
+var tests = map[string]struct {
+	pre			func()
+	forEach		func(int) error
 	flags		int
-	pre		func()
 	inputs		[]logCall
 	expected	[]string
-	post	func()
 }{
-	{
+	"00-all-types": {
+		// All types of messages
 		pre:	func() {
 			SetDebug(true)
 		},
@@ -24,16 +27,86 @@ var tests = []struct {
 			logCall{f: Debug, args: []any{0, "DEBUG"} },
 			logCall{f: Info, args: []any{1, "INFO"} },
 			logCall{f: Warn, args: []any{2, "WARNING"} },
-			logCall{f: Err, args: []any{3, "ERROR"} },
+			logCall{f: Err, args: []any{3, "ERROR (It's OK - this is testing error messages)"} },
 		},
 		expected: []string {
-			stubApp + `[` + stubPID + `]: <D> [#0] DEBUG log message`,
-			stubApp + `[` + stubPID + `]: [#1] INFO log message`,
-			stubApp + `[` + stubPID + `]: <WRN> [#2] WARNING log message`,
-			stubApp + `[` + stubPID + `]: <ERR> [#3] ERROR log message`,
+			stubApp + `[` + stubPID + `]: <D> Test #0 - DEBUG log message`,
+			stubApp + `[` + stubPID + `]: Test #1 - INFO log message`,
+			stubApp + `[` + stubPID + `]: <WRN> Test #2 - WARNING log message`,
+			stubApp + `[` + stubPID + `]: <ERR> Test #3 - ERROR (It's OK - this is testing error messages) log message`,
 		},
-		post:	func() {
+	},
+	"01-without-debug": {
+		// All types of messages except debug
+		flags:	NoFlags,
+		inputs:	[]logCall {
+			logCall{f: Debug, args: []any{0, "DEBUG"} },
+			logCall{f: Info, args: []any{1, "INFO"} },
+			logCall{f: Warn, args: []any{2, "WARNING"} },
+			logCall{f: Err, args: []any{3, "ERROR (It's OK - this is testing error messages)"} },
+		},
+		expected: []string {
+			stubApp + `[` + stubPID + `]: Test #1 - INFO log message`,
+			stubApp + `[` + stubPID + `]: <WRN> Test #2 - WARNING log message`,
+			stubApp + `[` + stubPID + `]: <ERR> Test #3 - ERROR (It's OK - this is testing error messages) log message`,
+		},
+	},
+	"02-with-NoPID": {
+		// All types of messages without PID
+		pre:	func() {
 			SetDebug(true)
+		},
+		flags:	NoPID,
+		inputs:	[]logCall {
+			logCall{f: Debug, args: []any{0, "DEBUG"} },
+			logCall{f: Info, args: []any{1, "INFO"} },
+			logCall{f: Warn, args: []any{2, "WARNING"} },
+			logCall{f: Err, args: []any{3, "ERROR (It's OK - this is testing error messages)"} },
+		},
+		expected: []string {
+			stubApp + `: <D> Test #0 - DEBUG log message`,
+			stubApp + `: Test #1 - INFO log message`,
+			stubApp + `: <WRN> Test #2 - WARNING log message`,
+			stubApp + `: <ERR> Test #3 - ERROR (It's OK - this is testing error messages) log message`,
+		},
+	},
+	"03-with-reopen": {
+		// All types of messages except with reopening before each message except first
+		pre:	func() {
+			SetDebug(true)
+		},
+		forEach:	func(inputN int) error {
+			if inputN == 0 {
+				// Skip
+				return nil
+			}
+
+			Warn("Test Reopen() #%d", inputN)
+
+			if err := Reopen(); err != nil {
+				return fmt.Errorf("Reopen() failed: %v", err)
+			}
+
+			// Set fake PID again
+			SetPID(stubPID)
+
+			return nil
+		},
+		flags:	NoFlags,
+		inputs:	[]logCall {
+			logCall{f: Debug, args: []any{0, "DEBUG"} },
+			logCall{f: Info, args: []any{1, "INFO"} },
+			logCall{f: Warn, args: []any{2, "WARNING"} },
+			logCall{f: Err, args: []any{3, "ERROR (It's OK - this is testing error messages)"} },
+		},
+		expected: []string {
+			stubApp + `[` + stubPID + `]: <D> Test #0 - DEBUG log message`,
+			stubApp + `[` + stubPID + `]: <WRN> Test Reopen() #1`,
+			stubApp + `[` + stubPID + `]: Test #1 - INFO log message`,
+			stubApp + `[` + stubPID + `]: <WRN> Test Reopen() #2`,
+			stubApp + `[` + stubPID + `]: <WRN> Test #2 - WARNING log message`,
+			stubApp + `[` + stubPID + `]: <WRN> Test Reopen() #3`,
+			stubApp + `[` + stubPID + `]: <ERR> Test #3 - ERROR (It's OK - this is testing error messages) log message`,
 		},
 	},
 }
